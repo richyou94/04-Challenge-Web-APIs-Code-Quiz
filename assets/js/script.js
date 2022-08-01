40c86cc92dfa30703ef4f23c8f7b65b88789a603
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-08-01 17:26:48 +0900

    made initla multiple choice box when I click startBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,16 @@ var quizBox = document.querySelector(".multiple-choice-container");
 var timeLeft = document.querySelector("#timeLeft");
 var secondsLeft = 100;
 var aBtnEl = document.querySelector(".aBtn")
+var setOne = ["Commonly used data types DO NOT include:", "1. strings", "2. booleans", "3. alerts", "4. numbers"]
+var setTwo = ["qOne", "aOne", "iaTwo", "iaThree", "iaFour"]
+var setThree = ["qOne", "aOne", "iaTwo", "iaThree", "iaFour"]
+var setFour = ["qOne", "aOne", "iaTwo", "iaThree", "iaFour"]
+var arraySets = [setOne, setTwo, setThree, setFour]
+var questionEl = document.querySelector(".multiple-choice-title")
+var choiceOne = document.querySelector(".aBtn")
+var choiceTwo = document.querySelector(".bBtn")
+var choiceThree = document.querySelector(".cBtn")
+var choiceFour = document.querySelector(".dBtn")
 
 function setTime() {
     var timerInterval = setInterval(function() {
@@ -11,14 +21,25 @@ function setTime() {
         timeLeft.textContent = secondsLeft;
     }, 1000);
 
-    aBtnEl.addEventListener("click", function() {
-        secondsLeft = secondsLeft - 10;
-    })
+    
 }
 
 startBtn.addEventListener("click", function() {
     initialBox.setAttribute("style", "display: none")
     quizBox.setAttribute("style", "display: block")
+    initialQuestion();
     setTime();
 });
 
+function checkAnswer(a, b) {
+    
+}
+
+function initialQuestion() {
+    questionEl.textContent = setOne[0];
+    choiceOne.textContent = setOne[1];
+    choiceTwo.textContent = setOne[2];
+    choiceThree.textContent = setOne[3];
+    choiceFour.textContent = setOne[4];
+
+}
