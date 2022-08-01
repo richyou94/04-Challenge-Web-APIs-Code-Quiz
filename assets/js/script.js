1d27abe28a49a3bd7fe3d7892fe4b3e4f13cbd0e
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-08-01 18:56:41 +0900

    add all the questions and answers in the arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,24 @@ var startBtn = document.querySelector(".startBtn");
 var initialBox = document.querySelector(".coding-quiz-container");
 var quizBox = document.querySelector(".multiple-choice-container");
 var timeLeft = document.querySelector("#timeLeft");
-var secondsLeft = 100;
+var secondsLeft = 75;
+
 var aBtnEl = document.querySelector(".aBtn")
 var bBtnEl = document.querySelector(".bBtn")
 var cBtnEl = document.querySelector(".cBtn")
 var dBtnEl = document.querySelector(".dBtn")
+
 var setOne = ["Commonly used data types DO NOT include:", "1. strings", "2. booleans", "3. alerts", "4. numbers"]
-var setTwo = ["qOne", "aOne", "iaTwo", "iaThree", "iaFour"]
-var setThree = ["qOne", "aOne", "iaTwo", "iaThree", "iaFour"]
-var setFour = ["qOne", "aOne", "iaTwo", "iaThree", "iaFour"]
-var arraySets = [setOne, setTwo, setThree, setFour]
+var setTwo = ["The condition in an if / else statement is enclosed within", "1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"]
+var setThree = ["Arrays in Javascript can be used to store ______.", "1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"]
+var setFour = ["String values must be enclosed within __________ when being assigned to variables.", "1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"]
+var setFive = ["A very useful tool used during development and debugging for printing content to the debugger is: ", "1. JavaScript", "2. terminal / bash", "3. for loops", "4. console.log"]
+
+
+var arraySets = [setOne, setTwo, setThree, setFour, setFive]
+
 var questionEl = document.querySelector(".multiple-choice-title")
-var choiceOne = document.querySelector(".aBtn")
-var choiceTwo = document.querySelector(".bBtn")
-var choiceThree = document.querySelector(".cBtn")
-var choiceFour = document.querySelector(".dBtn")
+
 
 
 function setTime() {
